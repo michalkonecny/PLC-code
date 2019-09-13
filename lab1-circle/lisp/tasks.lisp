3dfe476f5f905c00de98b1123286138702d39aa0
mikkonecny <mikkonecny@gmail.com>
2019-09-13 17:11:37 +0100

    fix tasks.lisp, add vscode workspace

--- lab1-circle/lisp/tasks.lisp
+++ lab1-circle/lisp/tasks.lisp
@@ -1,13 +1,13 @@
-;; code for Task :
+;; code for Task 1.2(a):
 (print (1 + 2))
 ;; the above expression throws an error
 
-;; code for Task :
+;; code for Task 1.2(b):
 (format t "type something: ")
 (setf input (read-line))
 (format t "you typed: ~a" input)
 
-;; code for Task :
-(let ((prg '(+ 1 n))) (print prg))
+;; code for Task 1.2(c):
+(let ((prg '(+ 1 n))) (print prg)      )
 ;; The above Lisp expression first defines a very simple program,
 ;; and then prints the program.
