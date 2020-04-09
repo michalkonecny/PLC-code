13c3d1b06e48d5a0d8e445a5a8ab8c68332cdff2
mikkonecny <mikkonecny@gmail.com>
2020-04-09 17:14:52 +0100

    u10 improved Java generics example

--- u10-modules/java/A.java
+++ u10-modules/java/A.java
@@ -3,11 +3,14 @@ public class A<E>
     private E e;
     // private E e = new E(); // BAD
 
-    public <T> void test(T t) { System.out.println("e = " + e + "; testIsE " + t ); }
+    public <T> void test(T t) { System.out.println("e = " + e + "; t = " + t ); }
 
     public static void main(String[] args){
-      A<Integer> a = new A<>();
-      a.e = 1;
-      a.test("hi!");
+      A<Integer> a1 = new A<>();
+      A<String> a2 = new A<>();
+
+      a1.e = 1;
+      a1.test("hi!");
+      a1.test(0);
     }
 }
